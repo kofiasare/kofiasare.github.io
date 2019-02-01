9debdc521ddb43b477ea593bb1bfbab866ad4f70
kofiasare <fremponggodfred79@gmail.com>
2019-02-01 10:28:00 +0000

    add intl tel input

--- projects/pages/login/assets/js/signup.js
+++ projects/pages/login/assets/js/signup.js
@@ -1,3 +1,20 @@
+$(() => {
+    $('form input[type=tel]').intlTelInput({
+        autoHideDialCode: false,
+        separateDialCode: false,
+        preferredCountries: ["gh"],
+        nationalMode: false,
+        initialCountry: "auto",
+        geoIpLookup: (callback) => {
+            $.get("https://ipinfo.io", () => {}, "jsonp")
+                .always(resp => {
+                    const country = (resp && resp.country) ? resp.country : "GH";
+                    callback(country);
+                });
+        }
+    });
+})
+
 NProgress.start();
 window.onload = () => {
     localStorage.clear();
